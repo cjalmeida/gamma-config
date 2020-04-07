d6dd1e17e2a12f3ac083700404e41786eaa47a1f
Cloves Almeida <almeida.cloves@bcg.com>
2020-04-07 01:26:28 +0000

    Bump version: 0.1.4 → 0.1.5

--- setup.py
+++ setup.py
@@ -57,7 +57,7 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/devex-br/gamma-config",
-    version="0.1.4",
+    version="0.1.5",
     zip_safe=False,
     entry_points={"gamma.cli": ["config-cli = gamma.config.cli"]},
 )
