fcfec568c80cab3dadace1afff656dc3e6a816f9
Cloves Almeida <almeida.cloves@bcg.com>
2020-06-05 12:41:17 +0000

    Bump version: 0.1.8 → 0.1.9

--- gamma/config/__init__.py
+++ gamma/config/__init__.py
@@ -5,4 +5,4 @@ dotenv.load_dotenv("config.env")
 
 from .config import get_config, get_meta_config, get_project_home, Config  # NOQA
 
-__version__ = "0.1.8"
+__version__ = "0.1.9"
--- setup.py
+++ setup.py
@@ -56,7 +56,7 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/devex-br/gamma-config",
-    version="0.1.8",
+    version="0.1.9",
     zip_safe=False,
     entry_points={"gamma.cli": ["config-cli = gamma.config.cli_plugin"]},
 )
