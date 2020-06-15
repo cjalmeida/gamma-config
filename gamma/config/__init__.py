64868518b7c6992b762eb5713495e994e534ca5d
Cloves Almeida <almeida.cloves@bcg.com>
2020-06-15 20:09:20 +0000

    update dotenv to load from PROJECT_HOME

--- gamma/config/__init__.py
+++ gamma/config/__init__.py
@@ -2,8 +2,9 @@ import dotenv
 
 from .config import Config, get_config, get_meta_config, get_project_home  # NOQA
 
-dotenv.load_dotenv("config.local.env")
-dotenv.load_dotenv("config.env")
+HOME = get_project_home()
+dotenv.load_dotenv(f"{HOME}/config.local.env")
+dotenv.load_dotenv(f"{HOME}/config.env")
 
 
 __version__ = "0.1.12"
