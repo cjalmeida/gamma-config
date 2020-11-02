6ba4dbd88bbec962e88df1c1163a6a1560b8011d
Cloves Almeida <almeida.cloves@bcg.com>
2020-11-02 18:32:42 -0300

    Bump version: 0.1.19 → 0.2.0

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import os
 
 from setuptools import find_namespace_packages, setup
 
-VERSION=os.getenv("VERSION", "0.1.19")
+VERSION=os.getenv("VERSION", "0.2.0")
 
 with open("README.rst") as readme_file:
     readme = readme_file.read()
