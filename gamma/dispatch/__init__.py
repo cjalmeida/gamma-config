5cefdd6e5b9966d44a3021f37542ffa92215331c
Cloves Almeida <almeida.cloves@bcg.com>
2021-06-23 23:29:32 -0300

    Update to lazily initialize pdb om install_breakpoint_hook

--- gamma/dispatch/__init__.py
+++ gamma/dispatch/__init__.py
@@ -1,13 +1,13 @@
 # flake8: noqa
-import pdb as orig_pdb
-
 from .dispatchsystem import DispatchError, dispatch
 from .parametric_types import ParametricMeta, Val, parametric
-
-pdb = orig_pdb.Pdb(skip=["gamma.dispatch.*", "typing", "abc", "inspect", "bdb", "pdb"])
 
 
 def install_breakpoint_hook():
     import sys
+    import pdb as orig_pdb
 
+    pdb = orig_pdb.Pdb(
+        skip=["gamma.dispatch.*", "typing", "abc", "inspect", "bdb", "pdb"]
+    )
     sys.breakpointhook = pdb.set_trace
