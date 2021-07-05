9a8524796e9dab2749b2931aee1bc514205a964d
Cloves Almeida <almeida.cloves@bcg.com>
2021-07-05 19:17:49 -0300

    fix (again) breakpoint hook on jupyter

--- gamma/dispatch/__init__.py
+++ gamma/dispatch/__init__.py
@@ -3,7 +3,22 @@ from .dispatchsystem import DispatchError, dispatch
 from .parametric_types import ParametricMeta, Val, parametric
 
 
+def _isjupyter():  # pragma: no cover
+    try:
+        shell = get_ipython().__class__.__name__  # type: ignore
+        if shell == "ZMQInteractiveShell":
+            return True  # Jupyter notebook or qtconsole
+        return False
+    except NameError:
+        return False  # Probably standard Python interpreter
+
+
 def install_breakpoint_hook():
+
+    if _isjupyter():
+        # we don't like Jupyter
+        return
+
     import sys
     import pdb as orig_pdb
 
