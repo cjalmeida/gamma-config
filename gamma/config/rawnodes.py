574977c0095a6b81ec1b86345c8ce890f6e3b37f
Cloves Almeida <almeida.cloves@bcg.com>
2022-09-02 08:35:09 -0300

    minor fixes

--- gamma/config/rawnodes.py
+++ gamma/config/rawnodes.py
@@ -48,9 +48,9 @@ def get_entry(node: MappingNode, key, *, default=...) -> Entry:
 
 
 @dispatch
-def is_equal(**kwargs) -> bool:
+def is_equal(a, b) -> bool:
     """Check if `a` is equal to `b`"""
-    return False
+    return a == b
 
 
 @dispatch
